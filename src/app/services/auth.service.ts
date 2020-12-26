81705543a05d782ff31694d396412fae54274c01
Jakub Kutka <jacobkutka@gmail.com>
2020-12-26 20:37:54 +0100

    Commit Neviem

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -22,11 +22,11 @@ export class AuthService {
     }, httpOptions);
   }
 
-  register(user: { username: string; email: string; password: string; }): Observable<any> {
+  register(user: { username: any; email: any; password: any; }): Observable<any> {
     return this.http.post(AUTH_API + 'signup', {
       username: user.username,
       email: user.email,
-      password: user.password
+      password: user.password,
     }, httpOptions);
   }
 }
